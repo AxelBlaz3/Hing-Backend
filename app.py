effeee88669dc750013f03cf4fd7191f578d6b4d
AxelBlaze <karthikgaddam4@gmail.com>
2021-10-05 13:41:13 +0530

    Update host
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ if __name__ == '__main__':
         http_server = WSGIServer(('127.0.0.1', 5050), app)
         http_server.serve_forever()
     else:
-        app.run(host='0.0.0.0', port=5050)  
+        app.run(host='127.0.0.1', port=5050)  
